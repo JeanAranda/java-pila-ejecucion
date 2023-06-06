429b2216c7f9ba53626e03d2f940d1dd01466749
JeanAranda <jarandar2014@gmail.com>
2023-06-06 06:21:47 -0500

    Atrapar errores Nullpointer y Arithmeti

--- src/Flujo.java
+++ src/Flujo.java
@@ -1,20 +1,45 @@
 public class Flujo {
-
-  public static void main(String[] args) {
-    System.out.println("Inicio de main");
-    metodo1();
-    System.out.println("Fin de main");
-  }
-  public static void metodo1(){
-    System.out.println("Inicio de metodo1");
-    metodo2();
-    System.out.println("Fim de metodo1");
-  }
-  public static void metodo2(){
-    System.out.println("Inicio de metodo2");
-    for(int i =1; i<= 5; i++){
-      System.out.println(i);
+	public static void main(String[] args) {
+        System.out.println("Inicio main");
+        metodo1();
+        System.out.println("Fin de main");
     }
-    System.out.println("Fin de metodo2");
-  }
+
+    private static void metodo1() {
+        System.out.println("Inicio de metodo1");
+        metodo2();
+        try {
+            metodo2();
+        } catch(ArithmeticException ex) {
+            System.out.println("ArithmeticException");
+        }
+        System.out.println("Fin de metodo1");
+    }
+
+    private static void metodo2() {
+        System.out.println("Inicio de metodo2");
+        for(int i = 1; i <= 5; i++) {
+            System.out.println(i);
+            try { //Intenta esto
+            	if (i == 3) {
+            		
+            		int num = 0;
+            		int resultado = i/num;
+            		System.out.println(resultado);
+            	}
+            		String test = null;
+            		System.out.println(test.toString());
+            }catch (ArithmeticException | NullPointerException exception) { //atrapa el error
+            	System.out.println("Atrapa Exception");
+            	System.out.println(exception.getMessage());
+            	exception.printStackTrace();
+				// TODO: handle exception
+			}
+        }
+       
+
+        System.out.println("Fin de metodo2");
+    }
+
+	
 }
---
+++ src/Principalcath.java
@@ -0,0 +1,16 @@
+
+public class Principalcath {
+	public static void main(String[] args) {
+
+        String nombre = "Alura"; 
+        System.out.println("ANTES");
+
+        try { 
+            System.out.println(nombre);
+        } catch(ArrayIndexOutOfBoundsException ex) { 
+            System.out.println("CATCH");
+        }
+
+        System.out.println("DESPUES"); 
+    }
+}
